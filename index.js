0f07c126a5db37ffb32feccde7aad327517e267e
Vladyslav Pastushenko <pastushenkovladislav21@gmail.com>
2023-01-16 14:29:55 +0200

    edited

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 /////////////
 // task #1 //
 /////////////
+
 let randomArray = new Array(100);
 
-for (let i = 0; i < randomArray.length; i++){
-    randomArray[i] = Math.floor(Math.random() * 101);
-}
 
-function sortByHeight(array) {
+for (let i = 0; i < randomArray.length; i++)
+    randomArray[i] = Math.floor(Math.random() * 101); 
+} 
+
+function sortByHeight(array) { 
     let sortedArray = array.slice().sort((a, b) => a - b);
     return sortedArray;
 }
 
-function valueGreaterThan50(array) {
-    let filteredArray = array.filter(element => element > 50);
+function valueGreaterThan50(array) { 
+    let filteredArray = array.filter(el => el > 50);
     return filteredArray;
 }
 
 let sortedArray = sortByHeight(randomArray);
-let filteredArray = valueGreaterThan50(sortedArray);
+let filteredArray = valueGreaterThan50(sortedArray);//
 
 
 console.log(filteredArray);
 
 /////////////
 // task #2 //
-/////////////
-let myArray = new Array(100);
+///////////////створюємо масив довжиною 100 пустих елементів
 
-function getRandomString() {
+
+const randomString = () => {
     let letters = 'abcdefghijklmnopqrstuvwxyz';
     let randomString = '';
     for (let i = 0; i < 5; i++) {
@@ -38,7 +40,7 @@ function getRandomString() {
 }
 
 for (let i = 0; i < myArray.length; i++) {
-    myArray[i] = getRandomString();
+    myArray[i] = randomString();
 }
 
 
@@ -54,4 +56,10 @@ function transformToObject(arr) {
     });
 }
 
-let transformedArray = transformToObject(myArray);;
+let transformedArray = transformToObject(myArray);
+
+console.log(sortedArrayTwo);
+console.log(transformedArray);
+
+
+
